b65f7a112433d35896388692d681d8075b074f30
Llamaful <medallan0118@gmail.com>
2025-02-08 00:17:57 -0500

    Found text render function

--- pdf-render.js
+++ pdf-render.js
@@ -3,19 +3,16 @@
 const { pdfjsLib } = globalThis;
 pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.10.38/pdf.worker.min.mjs';
 
-createPdf().then(url => {
-  console.log(url);
-  displayPDF(url);
-});
+displayPDF('https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf');
 
 function displayPDF(url) {
   var loadingTask = pdfjsLib.getDocument(url);
-  loadingTask.promise.then(function(pdf) {
-    console.log('PDF loaded');
+  loadingTask.promise.then(pdf => {
+    console.log('PDF loaded', pdf, pdf.numPages);
 
     // Fetch the first page
     var pageNumber = 1;
-    pdf.getPage(pageNumber).then(function(page) {
+    pdf.getPage(pageNumber).then(page => {
       console.log('Page loaded');
 
       var scale = 0.75;
@@ -42,8 +39,19 @@ function displayPDF(url) {
         viewport: viewport
       };
       var renderTask = page.render(renderContext);
-      renderTask.promise.then(function () {
+      renderTask.promise.then(() => {
         console.log('Page rendered');
+      });
+      
+      page.getTextContent().then(textContent => {
+        const textLayerDiv = document.getElementById('textlayer');
+        textLayerDiv.setAttribute('class', 'textLayer');
+        var textLayer =  pdfjsLib.renderTextLayer({
+          textContent: textContent,
+          container: textLayerDiv,
+          viewport: viewport
+        });
+        textLayer._render();
       });
     });
   }, function (reason) {
